0754daeed93cb8fa9062bcc8ebb5b1757e529c28
Andres1717 <mariaerq10@gmail.com>
2022-12-06 13:36:09 -0500

    Reto 4. AREA DE UN POLIGONO

---
+++ area-poligono.js
@@ -0,0 +1,41 @@
+/*
+Reto 4
+
+AREA DE UN POLIGONO
+
+Crea UNA UNICA FUNCION que sea capaz de calcular y
+retornar el area de un poligono.
+
+-La funcion recibira por parametro solo un poligono a la vez.
+-Los poligonos soportados serán triangulo, cuadrado y rectangulo.
+-Imprime el calculo del area de un poligono de cada tipo.
+*/
+
+function area (alto, ancho) {
+   return !ancho ? alto*alto : alto * ancho 
+}
+
+function triangulo (area) {
+   return area(3,2) / 2
+}
+
+function cuadrado (area) {
+    return area(4)
+}
+
+function rectangulo (area) {
+    return area(2, 7)
+}
+
+
+ function poligonArea (poligon) {
+    if (poligon === 'triangulo') {
+        return triangulo(area)
+    }else if (poligon === 'cuadrado') {
+        return cuadrado(area)
+    }else if (poligon === 'rectangulo') {
+        return rectangulo(area)
+    }   
+ }
+
+ console.log(poligonArea('cuadrado'))
